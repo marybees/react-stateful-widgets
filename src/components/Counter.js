5fd32daa84756db704b4f61069bbb43be16c82da
Mary Baker <meb601@gmail.com>
2020-08-10 17:42:02 -0400

    Add useState

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -46,10 +46,11 @@ STEP 6:
   This click handler needs to use 'setCount' to set the 'count' to be the initial number you chose.
 */
 
-import React from 'react'; /* STEP 0 */
+import React, {useState} from 'react'; /* STEP 0 */
 
 export default function Counter() {
   /* STEP 1 */
+
 
   const increment = () => {
     /* STEP 4 */
