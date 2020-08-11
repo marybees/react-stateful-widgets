c0acdd8063d5526d48ca6925a683088643548fa4
Mary Baker <meb601@gmail.com>
2020-08-10 22:23:21 -0400

    Complete Squares

--- src/components/Squares.js
+++ src/components/Squares.js
@@ -23,13 +23,21 @@ export default function Squares() {
   // Use the state hook twice, as we need two slices of state: 'squares' and
   // 'activeSquare'. One holds the _array_ of square ids, and the other keeps track
   // of the currently active square (if any).
+  const [ squares, setSquares ] = useState(listOfSquareIds);
+  console.log(squares)
+  const [ activeSquare, setActiveSquare ] = useState(null);
 
   const isActive = id => {
     // This is not a click handler but a helper, used inside the JSX, (See below)
     // and should return a class name of active if the id passed
     // matches the active square in state, empty string otherwise.
     // Right-click and "inspect element" on the square to see its effect.
-    return ''
+  
+      if(id === activeSquare) {
+        return 'active'
+      } else {
+        return ''
+      }
   };
 
   const markActive = id => {
@@ -37,6 +45,11 @@ export default function Squares() {
     // Set the id argument to be the active id in state
     // (unless it already is, in which case we should reset
     // the currently active square id back to initial state).
+    if (id === activeSquare) {
+      setActiveSquare(null)
+    } else {
+      setActiveSquare(id)
+    }
   };
 
   return (
@@ -47,11 +60,11 @@ export default function Squares() {
           /* Nasty bug! We should map over a slice of state, instead of 'listOfSquareIds'.
           We might say: "it works, though!" But if the list of squares is not state,
           we could never add squares, change squares or remove squares in the future. Fix!" */
-          listOfSquareIds.map(id =>
+          squares.map(id =>
             <div
               id={id}
               key={id}
-              className={`square${isActive(id)}`}
+              className={`square ${isActive(id)}`}
               onClick={() => markActive(id)}
             >
             </div>
